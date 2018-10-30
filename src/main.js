85ff4f59a405cc3d5305283eb7ae0b5aebc61d57
unknown <cwd794873588@sina.com>
2018-10-30 15:28:02 +0800

    mockjs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import './assets/styles/reset.css'
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias
 
 import Vue from 'vue'
 import {Button} from 'mint-ui'
 import App from './App'
 import router from './router'
 import store from './store'
+
+import './assets/styles/reset.css'
+import './mock/mockServer' //加载mockServer
 
 Vue.component(Button.name, Button)
 
