b6e38d3ec314dadefd619aecb3dab9f726f1d093
unknown <cwd794873588@sina.com>
2018-11-08 15:16:51 +0800

    vue-lazyload

--- src/main.js
+++ src/main.js
@@ -3,14 +3,19 @@
 
 import Vue from 'vue'
 import {Button} from 'mint-ui'
+import VueLazyload from 'vue-lazyload'
 import App from './App'
 import router from './router'
 import store from './store'
 
 import './assets/styles/reset.css'
 import './mock/mockServer' //加载mockServer
+import loading from './common/imgs/loading.gif'
 
 Vue.component(Button.name, Button)
+Vue.use(VueLazyload, { // 内部自定义一个指令lazy
+  loading
+})
 
 Vue.config.productionTip = false
 
